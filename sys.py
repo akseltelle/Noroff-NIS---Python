4f226f18e253c2515d04684306c0dde9e624d692
Aksel Telle <akseltelle98@gmail.com>
2021-01-07 10:23:00 +0100

    Updated

--- sys.py
+++ sys.py
@@ -1,19 +1,35 @@
 import sys
-import keyword
 
 # help()
 
-print("Python Keywords:")
-for theword in keyword.kwlist:
-    print(theword, end=", ") # kwlist contains a list of Python keywords
-print()
-    # iskeyword returns True or False depending on whether the word is a keyword
-print("Noroff is a Python keyword: ", keyword.iskeyword("Noroff"))
+# print("\n")
+#
+#
+# import keyword
+#
+# print("Python Keywords:")
+# for theword in keyword.kwlist:
+#     print(theword, end=", ") # kwlist contains a list of Python keywords
+# print()
+#     # iskeyword returns True or False depending on whether the word is a keyword
+# print("Noroff is a Python keyword: ", keyword.iskeyword("Noroff"))
+#
+# print("\n")
+#
+# print("Python Keywords:")
+# for thewordnis in keyword.kwlist:
+#     print(thewordnis, end=(", "))
+# print()
+# print("NIS is awesome, but not a keyword in Python: ", keyword.iskeyword("NIS"))
+#
+# print("\n")
 
-print("\n")
 
-print("Python Keywords:")
-for thewordnis in keyword.kwlist:
-    print(thewordnis, end=(", "))
-print()
-print("NIS is awesome, but not a keyword in Python: ", keyword.iskeyword("NIS"))
+import math
+
+# Declare values
+int_value = 2
+second_value = 25
+decimal_value = 5.3
+
+print("ceiling:", math.ceil(decimal_value))
