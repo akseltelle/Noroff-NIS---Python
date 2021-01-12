550d3f62072237d04fdef4e9e276a36cd2c384a5
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-12 09:19:20 +0100

    Created Module 3 Prax 2.py

---
+++ Module 3 Prax 2.py
@@ -0,0 +1,9 @@
+# School        : Noroff - Network and IT-Security
+# Author        : Aksel Telle
+# Email         : akseltelle98@gmail.com
+# Github        : https://github.com/akseltelle
+# This project  : https://github.com/akseltelle/Noroff-NIS---Python
+# Website       : https://fawdaw.com/
+import os                                                           # Import OS module to extract system information
+print("Welcome, " + os.getlogin() + "!")                            # Greet the user with a welcome message
+print("")
