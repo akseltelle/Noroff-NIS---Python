aac6b3f4bcab299a51361634c7a10762c4e495c7
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-12 09:26:27 +0100

    Updated

--- Module 3 Prax 2.py
+++ Module 3 Prax 2.py
@@ -7,3 +7,28 @@
 import os                                                           # Import OS module to extract system information
 print("Welcome, " + os.getlogin() + "!")                            # Greet the user with a welcome message
 print("")
+print("Opening thefile.txt")
+my_file = open("thefile.txt", "w")
+print("")
+print("Fle methods and properties")
+print("Name: ", my_file.name)
+print("Mode: ", my_file.mode)
+print("Closed: ", my_file.closed)
+print("Readable: ", my_file.readable())
+print("Writable: ", my_file.writable())
+print("")
+
+# Method 1
+write_me = "This is a test\n"
+my_file.write(write_me)
+
+# # Method 2
+# my_file.write("Hello there!\n")
+# my_file.write("Hello there!\n")
+# my_file.write("Hello there!\n")
+
+print("Closing thefile.txt")
+my_file.close()
+print("")
+print("Fle methods and properties")
+print("Closed: ", my_file.closed)
