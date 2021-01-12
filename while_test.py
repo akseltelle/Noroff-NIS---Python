a2ac45890c6448203fe03d4815bd63eae984d5c5
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-12 14:36:29 +0100

    Updated

---
+++ while_test.py
@@ -0,0 +1,15 @@
+# School        : Noroff - Network and IT-Security
+# Author        : Aksel Telle
+# Email         : akseltelle98@gmail.com
+# Github        : https://github.com/akseltelle
+# This project  : https://github.com/akseltelle/Noroff-NIS---Python
+# Website       : https://fawdaw.com/
+
+print("1, 2")
+string = input("String: ")
+if string == "1":
+    print("1")
+elif string == "2":
+    print("2")
+else:
+    exit("No Selection made")
