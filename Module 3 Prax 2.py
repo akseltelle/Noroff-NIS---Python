41d0b66bc20515f878698398dede8c4875d20cbb
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-12 09:39:31 +0100

    Updated

--- Module 3 Prax 2.py
+++ Module 3 Prax 2.py
@@ -6,29 +6,47 @@
 # Website       : https://fawdaw.com/
 import os                                                           # Import OS module to extract system information
 print("Welcome, " + os.getlogin() + "!")                            # Greet the user with a welcome message
-print("")
-print("Opening thefile.txt")
-my_file = open("thefile.txt", "w")
-print("")
-print("Fle methods and properties")
-print("Name: ", my_file.name)
-print("Mode: ", my_file.mode)
-print("Closed: ", my_file.closed)
-print("Readable: ", my_file.readable())
-print("Writable: ", my_file.writable())
-print("")
+print()
+# print("Opening thefile.txt")
+# my_file = open("thefile.txt", "w")
+# print()
+# print("Fle methods and properties")
+# print("Name: ", my_file.name)
+# print("Mode: ", my_file.mode)
+# print("Closed: ", my_file.closed)
+# print("Readable: ", my_file.readable())
+# print("Writable: ", my_file.writable())
+# print()
+#
+# # Method 1
+# write_me = "This is a test\n"
+# my_file.write(write_me)
+#
+# # Method 2
+# my_file.write("Hello there!\n")
+# my_file.write("Hello there!\n")
+# my_file.write("Hello there!\n")
+#
+# print("Closing thefile.txt")
+# my_file.close()
+# print()
+# print("Fle methods and properties")
+# print("Closed: ", my_file.closed)
 
-# Method 1
-write_me = "This is a test\n"
-my_file.write(write_me)
 
-# Method 2
-my_file.write("Hello there!\n")
-my_file.write("Hello there!\n")
-my_file.write("Hello there!\n")
 
-print("Closing thefile.txt")
+# my_file = open("thefile.csv", "w")
+# # write_me = "This is a test"
+# # my_file.write(write_me)
+#
+# my_file.write("Name,Description,Amount\n")
+# my_file.write("John Doe,First Customer,1000.00\n")
+# my_file.write("Jane Doe,Second Customer,2500.00\n")
+
+my_file = open("thefile.csv", "r")
+file_contents = my_file.readlines()
+print(file_contents)
+print()
+for line in file_contents:
+    print(line,end="")
 my_file.close()
-print("")
-print("Fle methods and properties")
-print("Closed: ", my_file.closed)
