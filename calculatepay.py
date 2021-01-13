d9da76deada5d419823f19950a24d8d121d32347
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-13 10:49:12 +0100

    Updated

--- calculatepay.py
+++ calculatepay.py
@@ -1,3 +1,10 @@
+# School        : Noroff - Network and IT-Security
+# Author        : Aksel Telle
+# Email         : akseltelle98@gmail.com
+# Github        : https://github.com/akseltelle
+# This project  : https://github.com/akseltelle/Noroff-NIS---Python
+# Website       : https://fawdaw.com/
+
 def hourly_pay(hours,rate):
     return hours * rate
 
