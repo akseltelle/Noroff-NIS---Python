46cfb7484ea64feacb741bb29d6352574032ebea
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-13 10:49:59 +0100

    Updated

--- Module 1 Prax 4.py
+++ Module 1 Prax 4.py
@@ -1,4 +1,11 @@
-#Source: Nils
+# School        : Noroff - Network and IT-Security
+# Author        : Aksel Telle
+# Email         : akseltelle98@gmail.com
+# Github        : https://github.com/akseltelle
+# This project  : https://github.com/akseltelle/Noroff-NIS---Python
+# Website       : https://fawdaw.com/
+
+#Source/credit: Nils (NIS)
 #!/usr/bin/env python3
 import os
 
