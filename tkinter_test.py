780cfedf863a1c33b29e3fa3e2328681e8a743ce
akseltelle <54355767+akseltelle@users.noreply.github.com>
2021-01-13 10:45:25 +0100

    Updated

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,3 +1,10 @@
+# School        : Noroff - Network and IT-Security
+# Author        : Aksel Telle
+# Email         : akseltelle98@gmail.com
+# Github        : https://github.com/akseltelle
+# This project  : https://github.com/akseltelle/Noroff-NIS---Python
+# Website       : https://fawdaw.com/
+
 import tkinter as tk
 
 class Application(tk.Frame):
